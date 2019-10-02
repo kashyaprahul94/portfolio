4ec0e8bf729ce37facbd1303aae200e6fdc93a71
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-02 23:24:49 +0200

    all set, ready to fly hopefully

--- pipelines/docker-tag.sh
+++ pipelines/docker-tag.sh
@@ -17,14 +17,12 @@ FULL_IMAGE_NAME=$REGISTRY/$DOCKER_USERNAME/$REPOSITORY_NAME/$DOCKER_IMAGE_NAME:$
 
 echo $GITHUB_CI_TOKEN | docker login $REGISTRY -u $DOCKER_USERNAME --password-stdin;
 
-echo $FULL_IMAGE_NAME;
+sudo yarn;
 
-# sudo yarn;
+sudo yarn build;
 
-# sudo yarn build;
+docker build --tag $LOCAL_IMAGE_NAME .;
 
-# docker build --tag $LOCAL_IMAGE_NAME .;
+docker tag $LOCAL_IMAGE_NAME $FULL_IMAGE_NAME;
 
-# docker tag $LOCAL_IMAGE_NAME $FULL_IMAGE_NAME;
-
-# docker push $FULL_IMAGE_NAME;
+docker push $FULL_IMAGE_NAME;
