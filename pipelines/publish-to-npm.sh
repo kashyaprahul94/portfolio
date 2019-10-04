937334394f5fae8db1a1aca18aee347bbb3488ee
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-04 22:18:07 +0200

    fix

--- pipelines/publish-to-npm.sh
+++ pipelines/publish-to-npm.sh
@@ -8,7 +8,7 @@ BRANCH=$(echo "${BRANCH/'refs-heads-'}");
 echo "//npm.pkg.github.com/:_authToken=$GITHUB_CI_TOKEN" > ./.npmrc
 
 if [ $BRANCH = "master" ]; then 
-  yarn lerna publish --exact --force-publish --message "Bump to - %s" --yes minor --registry "npm.pkg.github.com" --contents build;
+  yarn lerna publish --exact --force-publish --message "Bump to - %s" --yes minor --registry "https://npm.pkg.github.com" --contents build;
 else
-  yarn lerna publish --exact --force-publish --canary --yes --preid "$BRANCH" minor --registry "npm.pkg.github.com" --contents build;
+  yarn lerna publish --exact --force-publish --canary --yes --preid "$BRANCH" minor --registry "https://npm.pkg.github.com" --contents build;
 fi
