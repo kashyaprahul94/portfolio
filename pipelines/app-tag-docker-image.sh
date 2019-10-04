f84f02aaa4f4f148578a85b9cd7981c7d83bc20c
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-04 14:23:59 +0200

    tag needs login, beacuse of base image

--- pipelines/app-tag-docker-image.sh
+++ pipelines/app-tag-docker-image.sh
@@ -13,6 +13,8 @@ echo "Will be using TAG ---------> $TAG";
 
 LOCAL_IMAGE_NAME=$DOCKER_USERNAME/$REPOSITORY_NAME-$DOCKER_IMAGE_NAME:$TAG;
 
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin $DOCKER_REGISTRY;
+
 cd packages/app;
 
 sudo yarn;
