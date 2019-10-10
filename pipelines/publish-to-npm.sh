a85e04e848e5ccf550f4cc169ebf2a7fdb04be7c
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-10 20:05:42 +0200

    dummy

--- pipelines/publish-to-npm.sh
+++ pipelines/publish-to-npm.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-BRANCH=$GITHUB_REF;
+TAG=$BRANCH;
 
-BRANCH=$(echo "$BRANCH" | tr / _ | tr -d \[:space:\] | tr -cs \[:alnum:\] -);
-BRANCH=$(echo "${BRANCH/'refs-heads-'}");
+TAG=$(echo "$TAG" | tr / _ | tr -d \[:space:\] | tr -cs \[:alnum:\] -);
+TAG=$(echo "${TAG/'refs-heads-'}");
 
 echo "@kashyaprahul94:registry=https://$NPM_REGISTRY/" >> ./.npmrc;
 echo "//$NPM_REGISTRY/:_authToken=$NPM_TOKEN" >> ./.npmrc;
 
-if [ $BRANCH = "master" ]; then 
+
+git remote rm origin;
+git remote add origin "https://kashyaprahul94:$NPM_TOKEN@github.com/kashyaprahul94/portfolio.git";
+git fetch;
+      
+git config --global user.email "kashyaprahul94@gmail.com";
+git config --global user.name "Rahul Kashyap";
+
+
+if [ $TAG = "master" ]; then 
   echo "I shall release patch version";
   sudo yarn lerna publish patch --exact --force-publish --yes --registry "https://$NPM_REGISTRY" --message "Bump to - %s";
 else
