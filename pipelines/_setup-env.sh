a6b8050cf5e53fa6ad63ad3e9d6ecc50729bcaeb
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-12 12:38:38 +0200

    reattach head only in npm package step (#26)
    * reattach head only in npm package step
    
    * try
    
    * Bump to - v1.1.9-next.0 [skip ci]
    
    * try2
    
    * trrrry
    
    * Bump to - v1.1.9-next.1 [skip ci]
    
    * another try
    
    * Bump to - v1.1.9-next.2 [skip ci]
    
    * another thing
    
    * finally

--- pipelines/_setup-env.sh
+++ pipelines/_setup-env.sh
@@ -2,23 +2,29 @@
 
 # Branch
 BRANCH=$GITHUB_HEAD_REF; if [ -z "$BRANCH" ]; then BRANCH=$GITHUB_REF; fi;
+
 REMOTE_BRANCH=$BRANCH;
+ORIGIN_BRANCH=origin/$REMOTE_BRANCH;
+LOCAL_BRANCH=$(echo "${BRANCH/'refs/heads/'}");
 
-BRANCH=$(echo "$BRANCH" | tr / _ | tr -d \[:space:\] | tr -cs \[:alnum:\] -);
-BRANCH=$(echo "${BRANCH/'refs-heads-'}");
-
-ORIGIN_BRANCH=origin/$BRANCH;
+ESCAPED_BRANCH=$(echo "$LOCAL_BRANCH" | tr / _ | tr -d \[:space:\] | tr -cs \[:alnum:\] -);
 
 # Tag
-DOCKER_TAG=$BRANCH; if [ $DOCKER_TAG = "master" ]; then DOCKER_TAG="stable"; fi;
+DOCKER_TAG=$ESCAPED_BRANCH; if [ $DOCKER_TAG = "master" ]; then DOCKER_TAG="stable"; fi;
 
 
-echo "Branch ---------> $BRANCH";
-echo "Origin Branch ---------> $ORIGIN_BRANCH";
 echo "Remote Branch ---------> $REMOTE_BRANCH";
+echo "Origin Branch ---------> $ORIGIN_BRANCH";
+echo "Local Branch ---------> $LOCAL_BRANCH";
+echo "Escaped Branch ---------> $ESCAPED_BRANCH";
+
 echo "Docker TAG ---------> $DOCKER_TAG";
 
-echo ::set-env name=BRANCH::$BRANCH;
-echo ::set-env name=ORIGIN_BRANCH::$ORIGIN_BRANCH;
+
+
 echo ::set-env name=REMOTE_BRANCH::$REMOTE_BRANCH;
+echo ::set-env name=ORIGIN_BRANCH::$ORIGIN_BRANCH;
+echo ::set-env name=LOCAL_BRANCH::$LOCAL_BRANCH;
+echo ::set-env name=ESCAPED_BRANCH::$ESCAPED_BRANCH;
+
 echo ::set-env name=DOCKER_TAG::$DOCKER_TAG;
