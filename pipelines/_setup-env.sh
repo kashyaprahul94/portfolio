65a4d8a468d3685307b9044d9717b1fe754cd40d
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-12 10:39:34 +0200

    should be able to fix origin branch

--- pipelines/_setup-env.sh
+++ pipelines/_setup-env.sh
@@ -2,19 +2,23 @@
 
 # Branch
 BRANCH=$GITHUB_HEAD_REF; if [ -z "$BRANCH" ]; then BRANCH=$GITHUB_REF; fi;
-ORIGIN_BRANCH=$BRANCH;
+REMOTE_BRANCH=$BRANCH;
 
 BRANCH=$(echo "$BRANCH" | tr / _ | tr -d \[:space:\] | tr -cs \[:alnum:\] -);
 BRANCH=$(echo "${BRANCH/'refs-heads-'}");
+
+ORIGIN_BRANCH=origin/$BRANCH;
 
 # Tag
 DOCKER_TAG=$BRANCH; if [ $DOCKER_TAG = "master" ]; then DOCKER_TAG="stable"; fi;
 
 
-echo "Origin Branch ---------> $ORIGIN_BRANCH";
 echo "Branch ---------> $BRANCH";
+echo "Origin Branch ---------> $ORIGIN_BRANCH";
+echo "Remote Branch ---------> $REMOTE_BRANCH";
 echo "Docker TAG ---------> $DOCKER_TAG";
 
-echo ::set-env name=ORIGIN_BRANCH::$ORIGIN_BRANCH;
 echo ::set-env name=BRANCH::$BRANCH;
+echo ::set-env name=ORIGIN_BRANCH::$ORIGIN_BRANCH;
+echo ::set-env name=REMOTE_BRANCH::$REMOTE_BRANCH;
 echo ::set-env name=DOCKER_TAG::$DOCKER_TAG;
