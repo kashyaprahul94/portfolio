39df43497636c43c68c4dce2c87a6815fcabc964
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-13 18:02:28 +0200

    pipelines are contexuals now (#27)
    * pipelines are contexuals now
    
    * adjust renamed setup-env file as well

--- pipelines/app/tag-docker-image.sh
+++ pipelines/app/tag-docker-image.sh
@@ -6,6 +6,8 @@ LOCAL_IMAGE_NAME=$DOCKER_USERNAME/$REPOSITORY_NAME-$DOCKER_IMAGE_NAME:$TAG;
 
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin $DOCKER_REGISTRY;
 
-cd packages/app;
+pushd packages/app;
 
 docker build --tag $LOCAL_IMAGE_NAME .;
+
+popd;
