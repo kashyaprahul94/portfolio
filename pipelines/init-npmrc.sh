921169b18adcfde37c5d72426c76091290ccac2d
Rahul Kashyap <kashyaprahul94@gmail.com>
2020-09-20 17:59:03 +0530

    style is 1.1.15 now

--- pipelines/init-npmrc.sh
+++ pipelines/init-npmrc.sh
@@ -1,22 +1,19 @@
 #!/bin/bash -e
 
 readonly NPM_REGISTRY="$1"
-readonly NPM_TOKEN="$2"
-readonly NPM_USER_NAME="$3"
-readonly NPM_USER_EMAIL="$4"
-readonly DESTINATION="$5"
+readonly NPM_AUTH="$2"
+readonly NPM_USER_EMAIL="$3"
+readonly DESTINATION="$4"
 
 main() {
 
   echo "NPM_REGISTRY ---> $NPM_REGISTRY"
-  echo "NPM_TOKEN ---> ${#NPM_TOKEN}"
-  echo "NPM_USER_NAME ---> $NPM_USER_NAME"
+  echo "NPM_AUTH ---> ${#NPM_AUTH}"
   echo "NPM_USER_EMAIL ---> $NPM_USER_EMAIL"
   echo "DESTINATION ---> $DESTINATION"
 
   echo "@kashyaprahul94:registry=https://$NPM_REGISTRY" >> $DESTINATION/.npmrc
-  echo "//$NPM_REGISTRY:_password=$NPM_TOKEN" >> $DESTINATION/.npmrc
-  echo "//$NPM_REGISTRY:username=$NPM_USER_NAME" >> $DESTINATION/.npmrc
+  echo "//$NPM_REGISTRY:_auth=$NPM_AUTH" >> $DESTINATION/.npmrc
   echo "//$NPM_REGISTRY:email=$NPM_USER_EMAIL" >> $DESTINATION/.npmrc
   echo "//$NPM_REGISTRY:always-auth=true" >> $DESTINATION/.npmrc
 }
