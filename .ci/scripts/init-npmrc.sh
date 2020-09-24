6b89472620f7d3b620776345a1d7c905cd58d713
Rahul Kashyap <kashyaprahul94@gmail.com>
2020-09-24 17:00:41 +0530

    ci scripts go into ci folder

--- .ci/scripts/init-npmrc.sh
+++ .ci/scripts/init-npmrc.sh
@@ -5,6 +5,8 @@ readonly NPM_AUTH="$2"
 readonly NPM_USER_EMAIL="$3"
 readonly DESTINATION="$4"
 
+readonly SCOPE="kashyaprahul94"
+
 main() {
 
   echo "NPM_REGISTRY ---> $NPM_REGISTRY"
@@ -12,7 +14,7 @@ main() {
   echo "NPM_USER_EMAIL ---> $NPM_USER_EMAIL"
   echo "DESTINATION ---> $DESTINATION"
 
-  echo "@kashyaprahul94:registry=https://$NPM_REGISTRY" >> $DESTINATION/.npmrc
+  echo "@$SCOPE:registry=https://$NPM_REGISTRY" >> $DESTINATION/.npmrc
   echo "//$NPM_REGISTRY:_auth=$NPM_AUTH" >> $DESTINATION/.npmrc
   echo "//$NPM_REGISTRY:email=$NPM_USER_EMAIL" >> $DESTINATION/.npmrc
   echo "//$NPM_REGISTRY:always-auth=true" >> $DESTINATION/.npmrc
